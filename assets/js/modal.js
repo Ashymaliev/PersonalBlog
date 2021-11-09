bfdca9c43a730a20c49f185c169569d372ac5fd6
Ashymaliev <Nursultan-99kg@mail.ru>
2021-11-09 21:18:17 +0600

    add mobileNavjs

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,6 +3,7 @@ const body = document.body;
 const modalClose = document.querySelectorAll('.modal__close');
 const modal = document.querySelectorAll('.modal');
 
+
 modalBtn.forEach(item => {
     item.addEventListener('click', event => {
         let $this = event.currentTarget;
@@ -10,15 +11,14 @@ modalBtn.forEach(item => {
         let modal = document.getElementById(modalId);
         let modalContent = modal.querySelector('.modal__content');
         
-
         modalContent.addEventListener('click', event => {
             event.stopPropagation();
         });
-
+        
         modal.classList.add('show');
         body.classList.add('no-scroll');
-
-        setTimeout(function() {
+        
+        setTimeout(() => {
             modalContent.style.transform = 'none';
             modalContent.style.opacity = '1';
         }, 1);
@@ -26,28 +26,31 @@ modalBtn.forEach(item => {
     });
 });
 
+
 modalClose.forEach(item => {
     item.addEventListener('click', event => {
         let currentModal = event.currentTarget.closest('.modal');
-
+        
         closeModal(currentModal);
     });
 });
- 
+
+
 modal.forEach(item => {
     item.addEventListener('click', event => {
         let currentModal = event.currentTarget;
-
+        
         closeModal(currentModal);
     });
 });
 
-function closeModal(currentModal) {
-    let modalContent = modal.querySelectorAll('.modal__content');
-    modalContent.removeAttribute('style');
 
-    setTimeout(() => { 
+function closeModal(currentModal) {
+    let modalContent = currentModal.querySelector('.modal__content');
+    modalContent.removeAttribute('style');
+    
+    setTimeout(() => {
         currentModal.classList.remove('show');
         body.classList.remove('no-scroll');
     }, 200);
-};
+}
